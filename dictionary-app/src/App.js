1639f4a4e6059d64fd60b4c8b38271fe18b16224
cjp0421 <pedersencarol1@gmail.com>
2022-04-12 15:42:04 -0500

    continued setting up project files

--- dictionary-app/src/App.js
+++ dictionary-app/src/App.js
@@ -1,9 +1,18 @@
-import "./App.css";
+import "./styles.css";
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
+      <div className="container">
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="text-center">
+          This website was coded by Carol J. Pedersen. <br />
+          Check out the code on GitHub!
+        </footer>
+      </div>
     </div>
   );
 }
