34330826cde79b1aa60d277bcd90134e140a024a
cjp0421 <pedersencarol1@gmail.com>
2022-04-16 13:00:32 -0500

    removed alert of searched word

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -11,7 +11,6 @@ export default function Dictionary() {
 
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
 
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en/${keyword}`;
     axios.get(apiUrl).then(handleResponse);
